8284f84fb00b42216c92236e995d8d501025bb6f
zinnatullin <chingiz@zinnatullin.ru>
2026-07-20 00:19:08 +0300

    Add CLI configuration

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/true18/shortener/internal/config"
 	"github.com/true18/shortener/internal/server"
 )
 
 func main() {
-	if err := server.Run(); err != nil {
+	cfg, err := config.Parse(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := server.Run(cfg); err != nil {
 		log.Fatal(err)
 	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,19 +3,15 @@ package server
 import (
 	"net/http"
 
+	"github.com/true18/shortener/internal/config"
 	"github.com/true18/shortener/internal/handler"
 	"github.com/true18/shortener/internal/repository"
 )
 
-const (
-	Addr    = ":8080"
-	BaseURL = "http://localhost:8080"
-)
-
-func New() http.Handler {
-	return handler.New(repository.NewMemory(), BaseURL)
+func New(cfg config.Config) http.Handler {
+	return handler.New(repository.NewMemory(), cfg.BaseURL)
 }
 
-func Run() error {
-	return http.ListenAndServe(Addr, New())
+func Run(cfg config.Config) error {
+	return http.ListenAndServe(cfg.ServerAddress, New(cfg))
 }
